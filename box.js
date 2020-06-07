2809608101ca7085f15967f6ac6b285f4dd09e06
nissymh <65068034+nissymh@users.noreply.github.com>
2020-06-07 19:21:10 +0530

    Update box.js

--- box.js
+++ box.js
@@ -1,15 +1,23 @@
 class Box {
-    constructor(x, y, width, height) {
-      this.body = Bodies.rectangle(x, y, width, height);
-      this.width = width;
-      this.height = height;
-      
-      World.add(world, this.body);
+  constructor(x, y, width, height) {
+    var options = {
+        isStatic=true
     }
-    display(){
-      var pos = this.body.position;
-      rectMode(CENTER);
-      fill(255);
-      rect(pos.x,pos.y, this.width, this.height);
-    }
-  };
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(255);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
